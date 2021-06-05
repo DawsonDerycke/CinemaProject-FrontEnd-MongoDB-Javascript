ba0685d106f4ef3df4520ec4382e81c21a3dcbdb
DawsonDerycke <70524614+DawsonDerycke@users.noreply.github.com>
2021-06-05 20:06:20 +0200

    table

--- src/app/customers.service.ts
+++ src/app/customers.service.ts
@@ -3,13 +3,14 @@ import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http'
 //import { throwError } from 'rxjs';
 //import { retry, catchError } from 'rxjs/operators';
 import { Customers } from './models/customers';
-
+import users from '../assets/fake.json/customers.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomersService {
   url = "http://localhost:3000";
+  user: any = users;
 
   constructor(public httpClient: HttpClient) { }
 
--- src/app/list-users/list-users.component.ts
+++ src/app/list-users/list-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomersService } from '../customers.service';
 
 @Component({
   selector: 'app-list-users',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-users.component.scss']
 })
 export class ListUsersComponent implements OnInit {
+  users: any;
+  cols: any;
+  constructor(private customersService: CustomersService) { }
+  ngOnInit(){
 
-  constructor() { }
+    this.cols = [
+      { field: 'firstName', header: 'Prénom' },
+      { field: 'year', header: 'Age' },
+      { field: 'movie', header: 'Film' },
+      { field: 'seat', header: 'Siège' }
+  ];
 
-  ngOnInit(): void {
+    this.customersService.user;
+    this.users = this.customersService.user;
+    console.log(this.users);
   }
 
 }
